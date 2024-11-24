23d78e5b8ff0661c3b80d2eb67e30206644d5f5a
Uzuki-yin <994986608@qq.com>
2024-11-24 12:29:51 +0800

    Site updated: 2024-11-24 12:29:44

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1732389716497";
+const VERSION = "1732422507940";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
